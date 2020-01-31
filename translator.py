dec1e6d9637cf3e1389225bf3d53bf6d5a13faa0
mhashim6 <m6hashim@gmail.com>
2020-01-31 19:43:47 +0200

    initial commit.

---
+++ translator.py
@@ -0,0 +1,11 @@
+"""
+@author mhashim6
+"""
+import typing
+from googletrans import Translator
+
+_translator = Translator()
+
+
+def translate_eg_ar(sentences: [str]):
+    return list(map(lambda res: res.text, _translator.translate(sentences, src='en', dest='ar')))
